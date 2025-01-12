0c163ca346e915e4e932eaed896bca9e1f9138c5
Mradamello <adam.kalinko@gmail.com>
2025-01-12 06:52:00 +0100

    TextFadeAway
    Added interaction text fade away.

--- Assets/Scripts/UI/DisplayInfo.cs
+++ Assets/Scripts/UI/DisplayInfo.cs
@@ -18,6 +18,8 @@ public class DisplayInfo : MonoBehaviour
     {
         if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
         {
+            StopAllCoroutines();
+            PopUp.GetComponent<TextMeshPro>().color = new Color(PopUp.GetComponent<TextMeshPro>().color.r, PopUp.GetComponent<TextMeshPro>().color.g, PopUp.GetComponent<TextMeshPro>().color.b, 1);
             PopUp.SetActive(true);
         }
     }
@@ -26,7 +28,18 @@ public class DisplayInfo : MonoBehaviour
     {
         if(collision.gameObject.layer == LayerMask.NameToLayer("Player"))
         {
-            PopUp.SetActive(false);
+            //PopUp.SetActive(false);
+            StartCoroutine(FadePopup(1f, PopUp.GetComponent<TextMeshPro>()));
+        }
+    }
+
+    public IEnumerator FadePopup(float time, TextMeshPro text)
+    {
+        text.color = new Color(text.color.r, text.color.g, text.color.b, 1);
+        while(text.color.a > 0.0f)
+        {
+            text.color = new Color(text.color.r, text.color.g, text.color.b, text.color.a - (Time.deltaTime / time));
+            yield return null;
         }
     }
 }
